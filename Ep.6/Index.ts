904b0c0e084676939d45cf38aa0941667466a669
mbeawww <kankaweelmn@gmail.com>
2024-08-30 10:41:59 +0700

    Array Manipulation

---
+++ Ep.6/Index.ts
@@ -0,0 +1,34 @@
+// const score:number[] = [10,23,26,30,33]
+
+// let sum:number = 0
+// for (let i = 0; i < score.length; i++) {
+//     sum = sum + score[i] หาผลรวม
+// }
+// const avg = sum / score.length หาค่าเฉลี่ย
+// console.log(sum)
+// console.log(avg) 
+
+// const sName: string = 'n1, n2, n3, n4, n5, n6'
+
+// const sNameArray = sName.split(',') เปลีย่น string เป็น array
+// console.log(sNameArray.join('#')
+
+// const animal: string ='แมว,แมวน้ำ,เพนกวิ้น,หมา'
+
+// // const animalArray =  animal.split(',')
+// // console.log(animalArray)
+// // console.log(animalArray.pop()) //เอาตัวหลังออก
+// // console.log(animalArray)
+// // animalArray.push('บ้าน') //เอามาแทนที่ตัวหลัง
+// // console.log(animalArray)
+// // animalArray.shift() // เอาตัวหน้าออก
+// // console.log(animalArray)
+// // animalArray.unshift('แรด') //เอามาแทนที่ตัวหน้า
+// // console.log(animalArray)
+// // // console.log(animalArray.sort()) //เรียงอักษร ก-ฮ
+// // // console.log(animalArray.reverse()) //เรียงอักษร ฮ-ก
+// // console.log(animalArray.slice(0,2))
+
+const fruits = ["Banana","Orange","Apple","Mango"]
+console.log(fruits.sort())
+console.log(fruits.reverse())
